b3da9101997a5c752ae698ce365ddc777cfcd422
alex <bigbadmath@gmail.com>
2021-06-20 14:17:35 -0400

    less trivial wrap_fn example with notes

--- examples/wrapping.rs
+++ examples/wrapping.rs
@@ -1,31 +1,60 @@
 #![deny(warnings)]
-use warp::Filter;
+use std::{collections::HashMap, convert::Infallible, str::FromStr, sync::Arc};
+use tokio::sync::RwLock;
+use warp::{Filter, Rejection};
 
-fn hello_wrapper<F, T>(
-    filter: F,
-) -> impl Filter<Extract = (&'static str,)> + Clone + Send + Sync + 'static
+// convenience method that puts the store down into the chain
+fn with_store(
+    store: Arc<RwLock<HashMap<String, u64>>>,
+) -> impl Filter<Extract = (Arc<RwLock<HashMap<String, u64>>>,), Error = Infallible> + Clone {
+    warp::any().map(move || store.clone())
+}
+
+// async because getting the lock on the hashmap, simple hashmap incrementer
+async fn inc_path<T>(path: T, store: Arc<RwLock<HashMap<String, u64>>>) -> Result<(), Rejection>
 where
-    F: Filter<Extract = (T,), Error = std::convert::Infallible> + Clone + Send + Sync + 'static,
-    F::Extract: warp::Reply,
+    T: Send + Sync + ToString,
 {
-    warp::any()
-        .map(|| {
-            println!("before filter");
-        })
+    let mut s = store.write().await;
+    let c = s.entry(path.to_string()).or_insert(0);
+    *c += 1;
+    println!("Called path {:?} {} times.", path.to_string(), c);
+    Ok(())
+}
+
+// the wrapping function that takes F (along with the store) and returns F, applying inc_path
+fn inc_by_path_wrapper<F, T>(
+    filter: F,
+    store: Arc<RwLock<HashMap<String, u64>>>,
+) -> impl Filter<Extract = (T,)> + Clone + Send + Sync + 'static
+where
+    F: Filter<Extract = (T,), Error = Infallible> + Clone + Send + Sync + 'static,
+    F::Extract: warp::Reply,
+    // FromStr / ToString corresponds to the return type of map and recover in routes
+    T: FromStr + Send + Sync + 'static + ToString,
+{
+    println!("Called inc_by_path_wrapper during init.");
+    warp::path::param()
+        .and(with_store(store))
+        .and_then(inc_path)
+        // because inc_path returns () on success we need to dump it using untuple
         .untuple_one()
         .and(filter)
-        .map(|_arg| "wrapped hello world")
 }
 
 #[tokio::main]
 async fn main() {
+    let h = Arc::new(RwLock::new(HashMap::new()));
     // Match any request and return hello world!
     let routes = warp::any()
-        .map(|| "hello world")
+        .map(|| "hello world\n".to_string())
         .boxed()
-        .recover(|_err| async { Ok("recovered") })
-        // wrap the filter with hello_wrapper
-        .with(warp::wrap_fn(hello_wrapper));
+        .recover(|_err| async { Ok("recovered".to_string()) })
+        // unify is needed because we're actually passing routes into and back out, which creates Either
+        .unify()
+        // we capture the store into the closure using move
+        .with(warp::wrap_fn(move |f| inc_by_path_wrapper(f, h.clone())));
 
+    println!("Use curl localhost:3030/path_name to demo.");
     warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
